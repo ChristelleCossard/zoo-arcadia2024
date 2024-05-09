357c076ae90504f39ee5c976bb8a3118329c676a
Christelle Cossard <christelle.cossard10@gmail.com>
2024-05-09 15:37:12 +0200

    installation structure des dossiers

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+   // require_once 'templates/header.php';
+    
+   // require_once 'templates/home.php';
+
+    ECHO ("Bonjour!");
+   // require_once 'templates/footer.php';
+ ?> 
+
+
+
+
+
+
+
+
+
+      
