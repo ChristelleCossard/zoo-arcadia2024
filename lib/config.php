cd4a0dc1b4c7bd05847bd5e3eea3b98ebc5f1769
Christelle Cossard <christelle.cossard10@gmail.com>
2024-05-29 06:42:13 +0200

    ajout pages de base

---
+++ lib/config.php
@@ -0,0 +1,15 @@
+<?php
+
+define("_DOMAIN_", ".techtrendzstudi.local");
+//define("_DB_SERVER_", "mysql-cocoweb10.alwaysdata.net");
+//define("_DB_NAME_", "cocoweb10_studitech");
+//define("_DB_USER_", "cocoweb10");
+define("_DB_SERVER_", "mysql-cocodev.alwaysdata.net");
+define("_DB_NAME_", "cocodev_garage");
+define("_DB_USER_", "cocodev");
+define("_DB_PASSWORD_", "Vicky.10");
+define("_ASSETS_IMAGES_FOLDER_", "/assets/images/");
+define("_ARTICLES_IMAGES_FOLDER_", "/uploads/articles/");
+define("_HOME_ARTICLES_LIMIT_", 3);
+define("_ADMIN_ITEM_PER_PAGE", 10);
+define("_APP_EMAIL_", "christelle.cossard10@gmail.com");
---
+++ templates/footer.php
@@ -0,0 +1,80 @@
+</main>
+
+<hr>
+<footer>
+<h2 align="center"> Nos horaires d'ouvertures:</h2>
+               <br>
+<table class="table table-hover table-dark">
+  <thead>
+    <tr>
+      <th scope="col">JOUR</th>
+      <th scope="col"><h3>Lundi</h3></th>
+      <th scope="col"><h3>Mardi</h3></th>
+      <th scope="col"><h3>Mercredi</h3></th>
+      <th scope="col"><h3>Jeudi</h3></th>
+      <th scope="col"><h3>Vendredi</h3></th>
+      <th scope="col"><h3>Samedi</h3></th>
+      <th scope="col"><h3>Dimanche</h3></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Matin</th>
+      <td>FERME</td>
+      <td></td>
+      <td>12:00 - 14:00</td>
+      <td>12:00 - 14:00</td>
+      <td>12:00 - 14:00</td>
+      <td>12:00 - 14:00</td>
+      <td>12:00 - 14:00</td>
+    </tr>
+    <tr>
+      <th scope="row">Après-midi</th>
+      <td>FERME</td>
+      <td>12:00 - 14:00</td>
+      <<td>12:00 - 14:00</td>
+      <td>12:00 - 14:00</td>
+      <td>12:00 - 14:00</td>
+      <td>12:00 - 14:00</td>
+      <td>12:00 - 14:00</td>
+    </tr>
+    <tr>
+    
+  </tbody>
+</table>
+
+           
+            <div class="row footer">
+                <div class="d-flex justify-content-center">
+                    <div class="col-2">
+                        <ul class="list-unstyled text-center">
+                           
+                            <li><a class="nav-link" href="mailto:christelle.cossard10@gmail.com<">christelle.cossard10@gmail.com</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6 text-center copyright">
+                        <br>
+                        © 2023 
+		  <a class="nav-link" href="http://cocoweb10.fr/" target="_blank"> Christelle Cossard.</a>
+                    </div>
+                    <div class="col-2">
+                        <ul class="list-unstyled text-center">
+                            <li><a class="nav-link" href="legal.php">Mentions légales</a></li>
+                            <li><a class="nav-link" href="https://github.com/ChristelleCossard/garage-parrot" target="_blank">GITHUB du projet</a></li>
+                        <!--   -->
+                
+                      </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</footer>
+  </div>
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+
+</html>
