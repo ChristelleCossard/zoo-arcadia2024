c588fed0092da4202ee33cad2527b0efb046d834
Christelle Cossard <christelle.cossard10@gmail.com>
2024-07-15 06:53:36 +0200

    page des services

--- services.php
+++ services.php
@@ -6,35 +6,7 @@ require_once __DIR__ . "/templates/header.php";
 //$services = getServices($pdo);
 
 ?>
-<?php
-/*
-<?php 
-    require_once 'templates/header.php';
 
-    require_once('lib/product.php');
-
-
-    $products = getProducts($pdo);
-  ?>  
-  <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-    <h1 align="center"> Voici nos produits!</h1>
-  </div>
-
-  <div class="row">
-
-  <?php foreach ($products as $key => $product) { 
-     include('templates/product_partial.php');
-  } ?>
-
-</div>
-
-    <?php   
-    require_once 'templates/footer.php';
- ?> 
-
-
-*/
-?>
 
 
 <h1 align="center">Bienvenue sur la page des services!</h1>
@@ -46,8 +18,8 @@ require_once __DIR__ . "/templates/header.php";
 /*
 <div class="row">
 
-  <?php foreach ($products as $key => $product) { 
-     include('templates/product_partial.php');
+  <?php foreach ($services as $key => $service) { 
+     include('templates/service_partial.php');
   } ?>
 
 </div>
