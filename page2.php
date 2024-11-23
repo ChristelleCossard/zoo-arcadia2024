60d2b0478bd8096d017a96e0cab28b33ea126439
Christelle Cossard <christelle.cossard10@gmail.com>
2024-11-23 09:37:11 +0100

    ajout dans page 2

--- page2.php
+++ page2.php
@@ -5,6 +5,6 @@ require_once __DIR__ . "/templates/header.php";
 
 
 <h1 align="center">Bienvenue sur la page2!</h1>
-<p></p>
+<p>essai ajout</p>
 
 <?php require_once __DIR__ . "/templates/footer.php"; ?>
